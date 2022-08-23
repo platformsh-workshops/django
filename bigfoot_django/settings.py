445669ca54473015151155aeaa8fd8371831bd64
chadcarlson <chad.carlson@platform.sh>
2022-08-23 08:51:55 -0400

    Remove platformshconfig dependency.

--- bigfoot_django/settings.py
+++ bigfoot_django/settings.py
@@ -11,7 +11,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import json
+import base64
 from pathlib import Path
+
+# This variable should always match the primary database relationship name,
+#   configured in .platform.app.yaml.
+PLATFORMSH_DB_RELATIONSHIP="database"
+
 
 from platformshconfig import Config
 
@@ -135,17 +142,39 @@ STATIC_URL = '/static/'
 # Default STATIC_ROOT, i.e. for local testing.
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# Import some Platform.sh settings from the environment.
-config = Config()
-if config.is_valid_platform():
-    DEBUG = False
-    if config.appDir:
-        STATIC_ROOT = os.path.join(config.appDir, 'static')
-    if config.projectEntropy:
-        SECRET_KEY = config.projectEntropy
+#################################################################################
+# Platform.sh-specific configuration
+# Helper function for decoding base64-encoded JSON variables.
+@staticmethod
+def decode(variable):
+    """Decodes a Platform.sh environment variable.
+    Args:
+        variable (string):
+            Base64-encoded JSON (the content of an environment variable).
+    Returns:
+        An dict (if representing a JSON object), or a scalar type.
+    Raises:
+        JSON decoding error.
+    """
+    try:
+        if sys.version_info[1] > 5:
+            return json.loads(base64.b64decode(variable))
+        else:
+            return json.loads(base64.b64decode(variable).decode('utf-8'))
+    except json.decoder.JSONDecodeError:
+        print('Error decoding JSON, code %d', json.decoder.JSONDecodeError)
 
-    if not config.in_build():
-        db_settings = config.credentials('database')
+# Import some Platform.sh settings from the environment.
+if (os.getenv('PLATFORM_APPLICATION_NAME') is not None):
+    DEBUG = False
+    if (os.getenv('PLATFORM_APP_DIR') is not None):
+        STATIC_ROOT = os.path.join(os.getenv('PLATFORM_APP_DIR'), 'static')
+    if (os.getenv('PLATFORM_PROJECT_ENTROPY') is not None):
+        SECRET_KEY = config.projectEntropy
+    # Database service configuration, post-build only.
+    if (os.getenv('PLATFORM_ENVIRONMENT') is not None):
+        platformRelationships = decode(os.getenv('PLATFORM_RELATIONSHIPS'))
+        db_settings = platformRelationships[PLATFORMSH_DB_RELATIONSHIP][0]
         DATABASES = {
             'default': {
                 'ENGINE': 'django.db.backends.postgresql',
