dde9f59844e44ed2775666979f4bef50ab68dd45
chadcarlson <chad.carlson@platform.sh>
2022-08-23 08:56:56 -0400

    Remove import.

--- bigfoot_django/settings.py
+++ bigfoot_django/settings.py
@@ -19,9 +19,6 @@ from pathlib import Path
 #   configured in .platform.app.yaml.
 PLATFORMSH_DB_RELATIONSHIP="database"
 
-
-from platformshconfig import Config
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
